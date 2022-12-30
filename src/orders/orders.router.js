f563e7938643923647a6cf03d90529baf2d97ee9
daric-e <100322530+daric-e@users.noreply.github.com>
2022-12-29 22:53:37 -0700

    Started setting up orders router file

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -1,5 +1,11 @@
 const router = require("express").Router();
 
 // TODO: Implement the /orders routes needed to make the tests pass
+router
+    .route('/:orderId')
 
+
+
+router
+    .route('/')
 module.exports = router;
