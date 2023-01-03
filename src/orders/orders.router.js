7be97c35f5a12c092a7e29a5fdd4fb4fc58a44b1
daric-e <100322530+daric-e@users.noreply.github.com>
2023-01-02 21:02:47 -0700

    Added routes

--- src/orders/orders.router.js
+++ src/orders/orders.router.js
@@ -3,9 +3,14 @@ const router = require("express").Router();
 // TODO: Implement the /orders routes needed to make the tests pass
 router
 .route('/:orderId')
-
-
+.get(controller.read)
+.put(controller.update)
+.delete(controller.delete)
+.all(methodNotAllowed);
 
 router
 .route('/')
+.get(controller.list)
+.post(controller.create)
+.all(methodNotAllowed);
 module.exports = router;
