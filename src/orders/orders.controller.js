8b145e139fd0c1d294ba2b297873c6aeeef30854
daric-e <100322530+daric-e@users.noreply.github.com>
2023-01-02 21:02:37 -0700

    Finished orders controller

--- src/orders/orders.controller.js
+++ src/orders/orders.controller.js
@@ -8,7 +8,7 @@ const nextId = require("../utils/nextId");
 
 // TODO: Implement the /orders handlers needed to make the tests pass
 function list(req, res) {
-  res.json({ data: dishes });
+  res.json({ data: orders });
 }
 
 function create(req, res) {
@@ -18,26 +18,26 @@ function create(req, res) {
     deliverTo,
     mobileNumber,
     status,
-    dishes: [...dishes],
+    dishes: [...dishes]
   };
-  dishes.push(newOrder);
+  orders.push(newOrder);
   res.status(201).json({ data: newOrder });
 }
 
 function read(req, res) {
-  res.json({ data: res.locals.orders });
+  res.json({ data: res.locals.order });
 }
 
 function update(req, res) {
   const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
   const updateOrders = {
-    ...res.locals.orders,
+    ...res.locals.order,
     deliverTo,
     mobileNumber,
     status,
     dishes: [...dishes],
   };
-  res.json({ updateOrders });
+  res.json({ data: updateOrders });
 }
 
 function destroy(req, res) {
@@ -52,10 +52,10 @@ function destroy(req, res) {
 //middlewear
 function hasRequiredFields(req, res, next) {
   const { data: { deliverTo, mobileNumber, status, dishes } = {} } = req.body;
-  const neededFields = [deliverTo, mobileNumber, status, dishes];
-  for (const field of neededFields) {
-    if (!req.body.date[field]) {
-      next({ status: 400, message: `A '${field}' property is needed.` });
+  const requiredFields = ['deliverTo', 'mobileNumber', 'dishes'];
+  for (const field of requiredFields) {
+    if (!req.body.data[field]) {
+      return next({ status: 400, message: `A '${field}' property is required.` });
     }
   }
   next();
@@ -81,11 +81,70 @@ function validateQuantity(req, res, next) {
         error: `Dish ${index} must have a quantity that is an integer greater than 0`,
       });
     }
-  }
-  if (dishes[index].quantity < 1) {
-    return res.status(400).json({
-      error: `Dish ${index} must have a quantity that is an integer greater than 0`,
-    });
+    if (dishes[index].quantity < 1) {
+      return res.status(400).json({
+        error: `Dish ${index} must have a quantity that is an integer greater than 0`,
+      });
+    }
   }
   next();
+}
+
+//middlewear
+function orderExists(req, res, next) {
+  const { orderId } = req.params;
+  const foundOrder = orders.find((order) => +order.id === +orderId);
+  if (foundOrder) {
+    res.locals.order = foundOrder
+    return next();
+  }
+  next({
+    status: 404,
+    message: `dish id not found: ${req.params.orderId}`,
+  });
+}
+
+// middleware
+function validateId(req, res, next) {
+  const { orderId } = req.params;
+  const { data: { id } } = req.body
+  if(id) {
+      if (orderId !== id) {
+      return next({
+        status: 400,
+        message: `Dish id does not match route id. Dish: ${id}, Route: ${orderId}`,
+      });
+    }
+  }
+  next();
+}
+// middleware
+function validateStatus(req, res, next) {
+  const { data: { status } = {} } = req.body;
+  const validStatuses = ['delivered', 'out-for-delivery', 'pending']
+  if (!status) {
+    return res.status(400).json({ error: 'order must have a status' });
+  }
+  if (status === 'delivered') {
+    return res.status(400).json({ error: 'delivered order cannot be updated' });
+  }
+  if (!validStatuses.includes(status)) {
+    return res.status(400).json({ error: 'status invalid' });
+  }
+  next();
+}
+// middleware
+function validatePendingStatus(req, res, next) {
+  if (res.locals.order.status === 'pending') {
+    return next();
+  }
+  return res.status(400).json({ error: 'an order cannot be deleted unless it is pending' });
+}
+
+module.exports = {
+  list,
+  create: [hasRequiredFields, validateDishes, validateQuantity, create],
+  read: [orderExists, read],
+  update: [orderExists, hasRequiredFields, validateDishes, validateQuantity, validateId, validateStatus, update],
+  delete: [orderExists, validatePendingStatus, destroy],
 }
